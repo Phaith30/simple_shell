7179923ec7b1bc49525232171dfbddf537dc9e60
Phaith30 <phaithebruba30@gmail.com>
2023-05-20 14:32:53 -0700

    updated

---
+++ rellocate.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to da o1'block nameen
+ */
+
+void *_realloc - realloc(void *ptr, unsigned int old_size, unisigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
