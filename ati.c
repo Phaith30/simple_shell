74d83e5182d5b308ee640465bc54b51aa6211c8c
Phaith30 <phaithebruba30@gmail.com>
2023-05-22 07:06:27 -0700

    update

--- ati.c
+++ ati.c
@@ -22,3 +22,17 @@ int _atoi(char *s)
 
 	return (j * sign);
 }
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
--- enviroment.c
+++ enviroment.c
@@ -20,3 +20,34 @@ void _env(char **com)
 		s = *(environ + i);
 	}
 }
+
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments
+ * to maintain constant function prototype
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _mysetenv - initialize a new environment variable,
+ *  or modify an existing one 
+ *  @info: Structure containing potential arguments used to maintain
+ *     constant function prototype
+ * Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if(info->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2])0
+		return (0);
+	returrn (1);
+}
